ad63bc4e02e3da24738b06ce64a9088e482f782d
Jonathan John <44868912+jonathanjohn47@users.noreply.github.com>
2025-04-06 15:19:28 +0530

    User model using Freezed

--- lib/domain/models.dart
+++ lib/domain/models.dart
@@ -1,6 +1,28 @@
-class User {
-  final String id;
-  final String name;
+import 'package:freezed_annotation/freezed_annotation.dart';
 
-  User(this.id, this.name);
+part 'models.freezed.dart';
+part 'models.g.dart';
+
+@Freezed()
+class User with _$User {
+  const factory User({
+    required String id,
+    required String name,
+  }) = _User;
+
+  factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
+
+  @override
+  // TODO: implement id
+  String get id => throw UnimplementedError();
+
+  @override
+  // TODO: implement name
+  String get name => throw UnimplementedError();
+
+  @override
+  Map<String, dynamic> toJson() {
+    // TODO: implement toJson
+    throw UnimplementedError();
+  }
 }
---
+++ lib/domain/models.g.dart
@@ -0,0 +1,15 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'models.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_User _$UserFromJson(Map<String, dynamic> json) =>
+    _User(id: json['id'] as String, name: json['name'] as String);
+
+Map<String, dynamic> _$UserToJson(_User instance) => <String, dynamic>{
+  'id': instance.id,
+  'name': instance.name,
+};
