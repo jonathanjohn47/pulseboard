de851d4ed661283ecb216abdd47b736b2e722b98
Jonathan John <44868912+jonathanjohn47@users.noreply.github.com>
2025-04-07 07:36:12 +0530

    User model using Freezed update

--- lib/domain/models.g.dart
+++ lib/domain/models.g.dart
@@ -6,14 +6,10 @@ part of 'models.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-_User _$UserFromJson(Map<String, dynamic> json) => _User(
-  id: json['id'] as String,
-  name: json['name'] as String,
-  email: json['email'] as String,
-);
+_User _$UserFromJson(Map<String, dynamic> json) =>
+    _User(id: json['id'] as String, name: json['name'] as String);
 
 Map<String, dynamic> _$UserToJson(_User instance) => <String, dynamic>{
   'id': instance.id,
   'name': instance.name,
-  'email': instance.email,
 };
